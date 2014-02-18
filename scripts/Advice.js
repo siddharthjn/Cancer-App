bdb39307fe694ea849fa902c973fbefd6a3c4082
sleye <matt.triff@gmail.com>
2014-02-17 23:08:44 -0500

    Advice file refactored

--- scripts/Advice.js
+++ scripts/Advice.js
@@ -1,182 +1,209 @@
-function advicePage() {
-    if (localStorage.getItem("tbRecords") === null) {
-        alert("No records exist.");
-        var url = "#pagMenu";
-        $(location).attr('href', url);
+function advicePage()
+{
+  
+  if (localStorage.getItem("tbRecords") === null)
+  {
+    alert("No records exist.");
+
+    $(location).attr("href", "#pageMenu");
+  }
+  else 
+  {
+    $(location).attr("href", "#pageAdvice");
+    
+    var tbRecords=JSON.parse(localStorage.getItem("tbRecords"));
+    
+    var user=JSON.parse(localStorage.getItem("user"));
+    var TSHLevel=user.TSHRange;
+    
+    tbRecords.sort(compareDates);
+
+    var i=tbRecords.length-1;
+    
+    var TSH=tbRecords[i].TSH;
+    
+    var head=document.getElementById("AdviceCanvas");
+    var heading=head.getContext("2d");    
+    heading.font="22px Arial";
+
+    var c=document.getElementById("AdviceCanvas");
+    //Background color of canvas
+    var ctx=c.getContext("2d");
+    ctx.fillStyle="#c0c0c0";
+    ctx.fillRect(0,0,550,550);
+
+    //For writing if meter is in red zone
+    function WriteAdvice(level)
+    {
+      var adviceLine1="";
+      var adviceLine2="";
+
+      if(level=="red")
+      {
+        adviceLine1="Please consult with your family";
+        adviceLine2="physician urgently."; 
+      }
+      else if(level=="yellow")
+      {
+        adviceLine1="Contact family physician and recheck bloodwork";
+        adviceLine2="in 6-8 weeks.";
+      }
+      else if(level="green")
+      {
+        adviceLine1="Repeat bloodwork in 3-6 months.";
+      }
+
+      var r=document.getElementById("AdviceCanvas");
+      var rtx=r.getContext("2d");
+      rtx.font="22px Arial";
+      rtx.fillStyle="black";
+      rtx.fillText("Your TSH-level is " + TSH+".", 25, 320);
+      rtx.fillText(adviceLine1, 25, 380);
+      rtx.fillText(adviceLine2, 25, 410);      
     }
-    else {
-     var url = "#pageAdvice";
-        $(location).attr('href', url);
-        var tbRecords = JSON.parse(localStorage.getItem("tbRecords"));
-        var user = JSON.parse(localStorage.getItem("user"));
-        var TSHLevel = user.TSHRange;
-         tbRecords.sort(compareDates);
-        var i = tbRecords.length - 1;
-        var TSH = tbRecords[i].TSH;
-        var head = document.getElementById('AdviceCanvas');
-        var heading = head.getContext("2d");
-        heading.font = "22px Arial";
-        var c=document.getElementById("AdviceCanvas");
-        //Background color of canvas
-        var ctx=c.getContext("2d");
-        ctx.fillStyle="#c0c0c0";
-        ctx.fillRect(0,0,550,550);
+  
+    //For deciding what to write for given values of TSH Stage A
+    function StageAWrite()
+    {
+      if ((TSH >= 0.01) && (TSH <= 0.1)) 
+      {
+          WriteAdvice("green");
+      }
+      else if ((TSH > 0.1) && (TSH <= 0.5)) 
+      {
+          WriteAdvice("yellow");
+      }
+      else 
+      {
+          WriteAdvice("red");
+      }
+    }
 
-        //For writing if meter is in red zone
-        function WriteRed() {
-        var r=document.getElementById("AdviceCanvas");
-        var rtx=r.getContext("2d");
-        rtx.font = "22px Arial";
-        rtx.fillStyle = "black";
-        rtx.fillText("Your TSH-level is " + TSH+".", 25, 320);
-        rtx.fillText("Please consult with your family physician", 25, 380);
-        rtx.fillText("physician urgently. ", 25, 410);
-        }
-        //For writing if meter is in green zone
-        function WriteGreen() {
-        var g=document.getElementById("AdviceCanvas");
-        var gtx=g.getContext("2d");
-        gtx.font = "22px Arial";
-         gtx.fillStyle="black";
-         gtx.fillText("Your TSH-level is " + TSH+".", 25, 320);
-         gtx.fillText("Repeat bloodwork in 3-6 months. ", 25, 380);
+    function StageBWrite()
+    {
+      if ((TSH >= 0.1) && (TSH <= 0.5))
+      {
+        WriteAdvice("green");
+      }
+      else if ((TSH > 0.5) && (TSH <= 2.0))
+      {
+        WriteAdvice("yellow");
+      }
+      else if ((TSH >= 0.01) && (TSH < 0.1))
+      {
+        WriteAdvice("yellow");
+      }
+      else
+      {
+        WriteAdvice("red");
+      }
+    }
+           
+    function StageCWrite()
+    {
+      if ((TSH >= 0.35) && (TSH <= 2.0))
+      {
+        WriteAdvice("green");
+      }
+      else if ((TSH > 2) && (TSH <= 10))
+      {
+        WriteAdvice("yellow");
+      }
+      else if ((TSH >= 0.1) && (TSH < 0.35))
+      {
+        WriteAdvice("yellow");
+      }
+      else 
+      {
+        WriteAdvice("red");
+      }
+    }
 
-        }
-        //For writing if meter is in yellow zone
-        function WriteYellow() {
-            var y=document.getElementById("AdviceCanvas");
-            var ytx=y.getContext("2d");
-            ytx.font = "22px Arial";
-            ytx.fillStyle = "black";
-            ytx.fillText("Your TSH-level is " + TSH+".", 25, 320);
-            ytx.fillText("Contact family physician and recheck bloodwork", 25, 380);
-            ytx.fillText("in 6-8 weeks. ", 25, 410);
-            }
-        //For deciding what to write for given values of TSH Stage A
-         function StageAWrite() {
-            if ((TSH >= 0.01) && (TSH <= 0.1)) {
-                WriteGreen();
-                }
-            else if ((TSH > 0.1) && (TSH <= 0.5)) {
-                WriteYellow();
-                }
-            else {
-                WriteRed();
-                }
-            }
+    // Meter properties
+    function DrawMeter(g)
+    {
+      g.Set("chart.value.text.units.post", " mlU/L")
+        .Set("chart.value.text.boxed", false)
+        .Set("chart.value.text.size", 14)
+        .Set("chart.value.text.font", "Verdana")
+        .Set("chart.value.text.bold", true)
+        .Set("chart.value.text.decimals", 2)
+        .Set("chart.shadow.offsetx", 5)
+        .Set("chart.shadow.offsety", 5)
+        .Set("chart.scale.decimals", 2)
+        .Set("chart.title", "TSH LEVEL")
+        .Set("chart.radius", 250)
+        .Set("chart.centerx", 50)
+        .Set("chart.centery", 250)
+        .Draw();
+    }
 
-          function StageBWrite() {
-             if ((TSH >= 0.1) && (TSH <= 0.5)) {
-                WriteGreen();
-                }
-            else if ((TSH > 0.5) && (TSH <= 2.0)) {
-                WriteYellow();
-                }
-             else if ((TSH >= 0.01) && (TSH < 0.1)) {
-                WriteYellow();
-                }
-             else { 
-                WriteRed();
-                }
-             }
-               
-         function StageCWrite() {
-            if ((TSH >= 0.35) && (TSH <= 2.0)) {
-                WriteGreen();
-                }
-                else if ((TSH > 2) && (TSH <= 10)) {
-                WriteYellow();
-                }
-            else if ((TSH >= .1) && (TSH < .35)) {
-                WriteYellow();
-                }
-            else {
-                WriteRed();
-                }
-         }
-         // Meter properties
-     function DrawMeter(g)
-     {
+    function StageAMeter()
+    {
+      var cg=new RGraph.CornerGauge("AdviceCanvas", 0, 3, TSH)
+        .Set("chart.colors.ranges", [[0.5, 3, "red"], [0.1, 0.5, "yellow"], [0.01, 0.1, "#0f0"]]);
+      
+      DrawMeter(cg);
+      
+      //If TSH value is huge
+      if (TSH > 3)
+      {
+        var cg=new RGraph.CornerGauge("AdviceCanvas", 0,TSH,TSH)
+          .Set("chart.colors.ranges", [[0.5, 3, "red"], [0.1, 0.5, "yellow"], [0.01, 0.1, "#0f0"], [3.01, TSH, "red"]]);
+          DrawMeter(cg);
+      }
+    } 
 
-        g.Set('chart.value.text.units.post', ' mlU/L')
-            .Set('chart.value.text.boxed', false)
-            .Set('chart.value.text.size', 14)
-            .Set('chart.value.text.font', 'Verdana')
-            .Set('chart.value.text.bold', true)
-            .Set('chart.value.text.decimals', 2)
-            .Set('chart.shadow.offsetx', 5)
-            .Set('chart.shadow.offsety', 5)
-            .Set('chart.scale.decimals', 2)
-            .Set('chart.title', 'TSH LEVEL')
-            .Set('chart.radius', 250)
-            .Set('chart.centerx', 50)
-            .Set('chart.centery', 250)
-            .Draw();
-     }
-         function StageAMeter()
-         {
-         var cg = new RGraph.CornerGauge('AdviceCanvas', 0,3,TSH)
-            .Set('chart.colors.ranges', [[.5,3, 'red'], [.1,.5,'yellow'], [.01,.1, '#0f0']]);
-        DrawMeter(cg);
-            //If TSH value is huge
-            if (TSH > 3)
-            {
-            var cg = new RGraph.CornerGauge('AdviceCanvas', 0,TSH,TSH)
-            .Set('chart.colors.ranges', [[.5,3, 'red'], [.1,.5,'yellow'], [.01,.1, '#0f0'], [3.01, TSH, 'red']]);
-           DrawMeter(cg);
-            }
-         }
-         
-         function StageBMeter() {
-         var bcg = new RGraph.CornerGauge('AdviceCanvas', 0,3,TSH)
-            .Set('chart.colors.ranges', [[2.01,3, 'red'], [.51,2,'yellow'], [.1,.5, '#0f0'], [.01,.1,'yellow']]);
+    function StageBMeter()
+    {
+      var bcg=new RGraph.CornerGauge("AdviceCanvas", 0, 3, TSH)
+        .Set("chart.colors.ranges", [[2.01, 3, "red"], [0.51, 2, "yellow"], [0.1, 0.5, "#0f0"], [0.01, 0.1, "yellow"]]);
+      DrawMeter(bcg);
+      
+      if (TSH > 3)
+      {
+        var bcg=new RGraph.CornerGauge("AdviceCanvas", 0, TSH, TSH)
+          .Set("chart.colors.ranges", [[2.01, 3, "red"], [0.51, 2,"yellow"], [0.1, 0.5, "#0f0"], [0.01, 0.1, "yellow"], [3, TSH, "red"]]);
         DrawMeter(bcg);
-
-            if (TSH > 3)
-            {
-            var bcg = new RGraph.CornerGauge('AdviceCanvas', 0,TSH,TSH)
-            .Set('chart.colors.ranges', [[2.01,3, 'red'], [.51,2,'yellow'], [.1,.5, '#0f0'], [.01,.1, 'yellow'],[3,TSH,'red']]);
-           DrawMeter(bcg);
-            }
-         }
-         
-         function StageCMeter() {
-         var ccg = new RGraph.CornerGauge('AdviceCanvas', 0,15,TSH)
-            .Set('chart.colors.ranges', [[10.01,15, 'red'], [2.01,10,'yellow'], [.35,2, '#0f0'], [.1,.34,'yellow']]);
-        DrawMeter(ccg);
-
-            if (TSH > 15)
-            {
-            var ccg = new RGraph.CornerGauge('AdviceCanvas', 0,TSH,TSH)
-            .Set('chart.colors.ranges', [[10.01,15, 'red'], [2.01,10,'yellow'], [.35,2, '#0f0'], [.1,.34, 'yellow'],[15.01,TSH,'red']]);
-           DrawMeter(ccg);
-            }
-     }
+      }
+    }
      
+    function StageCMeter()
+    {
+      var ccg=new RGraph.CornerGauge("AdviceCanvas", 0, 15, TSH)
+        .Set("chart.colors.ranges", [[10.01, 15, "red"], [2.01, 10,"yellow"], [0.35, 2, "#0f0"], [0.1,.34,"yellow"]]);
+      DrawMeter(ccg);
+
+      if (TSH > 15)
+      {
+        var ccg=new RGraph.CornerGauge("AdviceCanvas", 0, TSH, TSH)
+          .Set("chart.colors.ranges", [[10.01, 15, "red"], [2.01, 10, "yellow"], [0.35, 2, "#0f0"], [0.1, 0.34, "yellow"], [15.01, TSH, "red"]]);
+        DrawMeter(ccg);
+      }
+    }
+   
     // main 
     if (TSHLevel == "StageA")
     {
-        StageAWrite();
-        heading.fillText("Your target TSH range is: '0.01-0.1 mlU/L'", 25, 350); 
-        StageAMeter();
+      StageAWrite();
+      heading.fillText("Your target TSH range is: 0.01-0.1 mlU/L", 25, 350); 
+      StageAMeter();
     }
-
     else if (TSHLevel == "StageB")
-    {
-        
-        StageBWrite();
-        heading.fillText("Your target TSH range is: '0.1-0.5 mlU/L'", 25, 350);
-        StageBMeter();
+    {    
+      StageBWrite();
+      heading.fillText("Your target TSH range is: 0.1-0.5 mlU/L", 25, 350);
+      StageBMeter();
     }
     else if (TSHLevel == "StageC")
     {
-        StageCWrite();
-        heading.fillText("Your target TSH range is: '0.35-2.0 mlU/L'", 25, 350);
-        StageCMeter();
+      StageCWrite();
+      heading.fillText("Your target TSH range is: 0.35-2.0 mlU/L", 25, 350);
+      StageCMeter();
     }
     else
     {
-    alert("No records exist.");
+      alert("No records exist.");
     }
-
-}}
+  }
+}
